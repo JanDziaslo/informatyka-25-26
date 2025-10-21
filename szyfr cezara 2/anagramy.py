0d586589b57eab451c113d6e0bff00b9b2ba0e54
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2025-10-21 09:11:07 +0200

    Delete szyfr cezara 2 directory

--- szyfr cezara 2/anagramy.py
+++
@@ -1,8 +0,0 @@
-slowo1 = input("Podaj pierwsze słowo: ")
-slowo2 = input("Podaj drugie słowo: ")
-slowo3 = input("Podaj trzecie słowo: ")
-
-if sorted(slowo1) == sorted(slowo2) == sorted(slowo3):
-    print("Słowa są anagramami")
-else:
-    print("Słowa nie są anagramami")
