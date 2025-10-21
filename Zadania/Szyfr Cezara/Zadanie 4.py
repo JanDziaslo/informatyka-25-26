18aa50b2c617cbd17334aeae2353effa233f1693
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2025-10-21 09:10:53 +0200

    Delete Zadania directory

--- Zadania/Szyfr Cezara/Zadanie 4.py
+++
@@ -1,5 +0,0 @@
-# Sprawdź w internecie częstość występowania poszczególnych liter w języku angielskim. Omówimy w tym
-# temacie program łamania szyfru Cezara zmodyfikuj tak, aby deszyfrował teksty w języku angielskim. Sprawdź
-# działanie programu.
-
-# No kodu na sprawdzianie nie będzie raczej więc zadanie pomijam
--- Zadania/Szyfr Cezara/zadanie10.py
+++
@@ -1,8 +0,0 @@
-#Napisz program wczytujący trzy słowa i sprawdzający, czy są one anagramami.
-slowo1 = input("Podaj pierwsze słowo: ")
-slowo2 = input("Podaj drugie słowo: ")
-slowo3 = input("Podaj trzecie słowo: ")
-if sorted(slowo1) == sorted(slowo2) == sorted(slowo3):
-    print("Słowa są anagramami.")
-else:
-    print("Słowa nie są anagramami.")
