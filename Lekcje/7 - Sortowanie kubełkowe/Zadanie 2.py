579bc9ecfd0bc4e24208223947cb61bb60ee44ac
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2025-12-12 14:52:36 +0000

    Poprawienie funkcji sortowania z zadania 2 - Sortowanie alfabetyczne -> Sortowanie po długosci

--- Lekcje/7 - Sortowanie kubełkowe/Zadanie 2.py
+++ Lekcje/7 - Sortowanie kubełkowe/Zadanie 2.py
@@ -1,27 +1,23 @@
-n = int(input("Podaj liczbę elementów tablicy: "))
-
-tablica_slowa = []
-for i in range(n):
-    slowo = input(f"Podaj wyraz {i+1}: ")
-    tablica_slowa.append(slowo)
-
-min = min(tablica_slowa)
-max = max(tablica_slowa)
-
-kubelki = [[] for _ in range(26)]
+n = int(input("Podaj liczbę wyrazów: "))
+tablica = [0] * n
 
 for i in range(n):
-    pierwsza_litera = tablica_slowa[i][0].lower()
-    indeks = ord(pierwsza_litera) - ord('a')
-    if 0 <= indeks < 26:
-        kubelki[indeks].append(tablica_slowa[i])
+    tablica[i] = input(f"Wprowadź {i + 1} wyraz: ")
+
+min = min(len(wyraz) for wyraz in tablica)
+max = max(len(wyraz) for wyraz in tablica)
+
+liczniki = [[] for _ in range(max - min + 1)]
+
+for i in range(n):
+    liczniki[len(tablica[i]) - min].append(tablica[i])
 
 j = 0
 
-for i in range(26):
-    for slowo in sorted(kubelki[i]):
-        tablica_slowa[j] = slowo
+for i in range(len(liczniki)):
+    while len(liczniki[i]) > 0:
+        tablica[j] = liczniki[i][0]
+        liczniki[i] = liczniki[i][1:]
         j += 1
 
-print("Posortowana tablica:")
-print(tablica_slowa)
+print(tablica)
