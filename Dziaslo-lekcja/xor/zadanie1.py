b081946d8508fd32ed85f688c9f885d40d0d0f4c
JanDziaslo <u22_bartosznikitiuk@zsp1.siedlce.pl>
2026-05-18 14:36:09 +0200

    xor

--- Dziaslo-lekcja/xor/zadanie1.py
+++ Dziaslo-lekcja/xor/zadanie1.py
@@ -1,3 +1,11 @@
+def szyfruj(tekst, klucz):
+    lista = []
+    lista[:0] = tekst
+    for i in range(len(tekst)):
+        lista[i] = chr(ord(lista[i]) ^ klucz)
+    return ''.join(lista)
+
+
 with open("slowa1.txt") as f:
     lines = f.readlines()
 
@@ -6,8 +14,7 @@ for line in lines:
     parts = line.strip().split(" ", 1)
     klucz = int(parts[0])
     slowo = parts[1]
-    zaszyfrowane = "".join(chr(ord(c) ^ klucz) for c in slowo)
-    wynik.append(zaszyfrowane)
+    wynik.append(szyfruj(slowo, klucz))
 
 with open("wynik4.txt", "w") as f:
     f.write("\n".join(wynik) + "\n")
